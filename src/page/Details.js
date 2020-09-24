b25c50ffd36b6870333bccc9f1646f07accf11ae
luckmer <woszczo04@gmail.com>
2020-09-24 13:14:54 +0200

    details page

--- src/page/Details.js
+++ src/page/Details.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { StoreContext } from "../store/index";
+import styled from "styled-components";
 
-function Details() {
-    return <div></div>;
+const Container = styled.section`
+    padding: 10vh 0 0 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+function Details({ match }) {
+    const [detailData, setDetailData] = useState({
+        DetailsData: [],
+    });
+    const {
+        DATA: [table, setTable],
+    } = useContext(StoreContext);
+    const { DetailsData } = detailData;
+
+    useEffect(() => {
+        let projectId = table.data.filter((el) => el.id === match.params.id);
+        setDetailData({ DetailsData: projectId });
+    }, [match.params.id, table.data]);
+
+    console.log(match);
+
+    return (
+        <Container>
+            {DetailsData.map(({ title, id }) => (
+                <div key={id}>{title}</div>
+            ))}
+        </Container>
+    );
 }
 
 export default Details;
