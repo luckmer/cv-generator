047e959bf27eaaa0bd084b502369e3cd1d8f27a9
luckmer <woszczo04@gmail.com>
2020-09-24 14:10:49 +0200

    store update

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,14 @@ export default ({ children }) => {
     const [table, setTable] = useState({
         data: [],
     });
-    const store = {
-        DATA: [table, setTable],
+    const [detailData, setDetailData] = useState({
+        DetailsData: [],
+    });
+
+    const clearTableData = () => {
+        const clearState = table.data;
+        clearState.splice(clearState, 1);
+        setTable({ data: clearState });
     };
     useEffect(() => {
         const store = JSON.parse(localStorage.getItem("TableData"));
@@ -17,6 +23,12 @@ export default ({ children }) => {
     useEffect(() => {
         localStorage.setItem("TableData", JSON.stringify(table));
     }, [table]);
+
+    const store = {
+        clearTableData,
+        DETAILS: [detailData, setDetailData],
+        DATA: [table, setTable],
+    };
 
     return (
         <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
