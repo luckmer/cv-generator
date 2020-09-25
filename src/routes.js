1b415dd2ebc377245ebb7bb95105bb646a764fcf
luckmer <woszczo04@gmail.com>
2020-09-25 11:06:33 +0200

    tiny fix

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Details from "./page/Details";
 import IncorrectPage from "./page/404";
 import LandingPage from "./page/LandingPage";
+import Context from "./components/Update/Context";
 
 export const routes = [
     { path: "/", Component: LandingPage, name: "home" },
-    { path: "/details/:id", Component: Details, name: "home" },
+    { path: "/details/:id", Component: Details, name: "details" },
+    { path: "/edit", Component: Context, name: "edit" },
     { path: "*", Component: IncorrectPage, name: "404" },
 ];
