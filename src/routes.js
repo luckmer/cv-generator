41ed9b93de4183e9b9216eb12fa181e6bd08101d
luckmer <woszczo04@gmail.com>
2020-09-27 14:22:11 +0200

    new page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import Details from "./page/Details";
 import IncorrectPage from "./page/404";
 import LandingPage from "./page/LandingPage";
-import Context from "./components/Update/Context";
+import Context from "./page/Context";
 
 export const routes = [
     { path: "/", Component: LandingPage, name: "home" },
     { path: "/details/:id", Component: Details, name: "details" },
-    { path: "/edit", Component: Context, name: "edit" },
+    { path: "/edit/:id", Component: Context, name: "details" },
     { path: "*", Component: IncorrectPage, name: "404" },
 ];
