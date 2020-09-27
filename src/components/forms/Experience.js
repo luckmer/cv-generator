e563e01f0bd4c973f99f7168b2bdd0611be9a5f2
luckmer <woszczo04@gmail.com>
2020-09-27 14:24:08 +0200

    experience page

--- src/components/forms/Experience.js
+++ src/components/forms/Experience.js
@@ -1,6 +1,18 @@
 import React from "react";
 
+// const InitialExperience = {
+// workExperience: "",
+// education: "",
+// languageKnowledge: "",
+// TrainingCoursesCertificates: "",
+// professionalProfile: "",
+// };
 function Experience() {
+    // const [expInitial, setExpInitial] = useState({
+    //     expDataInit: [],
+    // });
+    // const [state, setState] = useState(InitialExperience);
+
     return <div></div>;
 }
 
