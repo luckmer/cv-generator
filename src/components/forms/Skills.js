af93c80807ea0d754336bd799748f640a066f853
luckmer <woszczo04@gmail.com>
2020-09-27 14:24:13 +0200

    skills page

--- src/components/forms/Skills.js
+++ src/components/forms/Skills.js
@@ -1,6 +1,19 @@
 import React from "react";
 
+// const InitialSkills = {
+// skills: "",
+// additionalActivity: "",
+// interests: "",
+// Link: "",
+// ConsentToTheProcessingOfPersonalData: "",
+// };
+
 function Skills() {
+    // const [skillInitial, setSkillInitial] = useState({
+    //     SkillDataInit: [],
+    // });
+    // const [state, setState] = useState(InitialSkills);
+
     return <div></div>;
 }
 
