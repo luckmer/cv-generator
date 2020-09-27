b59e00b1bc1d2f76e3c214d6b1a2236ef7ae095b
luckmer <woszczo04@gmail.com>
2020-09-27 14:22:19 +0200

    details view

--- src/page/Details.js
+++ src/page/Details.js
@@ -1,8 +1,7 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { StoreContext } from "../store/index";
-import Context from "../components/Update/Context";
 import styled from "styled-components";
-
+import DetailFilter from "../hooks/DetailFilter";
 const Container = styled.section`
     padding: 10vh 0 0 0;
     width: 100%;
@@ -17,13 +16,10 @@ function Details({ match }) {
         DETAILS: [detailData, setDetailData],
         DATA: [table],
     } = useContext(StoreContext);
+
+    DetailFilter(table, match, setDetailData);
+
     const { DetailsData } = detailData;
-
-    useEffect(() => {
-        let projectId = table.data.filter((el) => el.id === match.params.id);
-        setDetailData({ DetailsData: projectId });
-    }, [match.params.id, table.data, setDetailData]);
-
     return (
         <Container>
             {DetailsData.map(({ title, id }) => (
