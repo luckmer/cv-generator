3c7e9894cacb02cf7c2cc3057aa31da1663002d3
luckmer <woszczo04@gmail.com>
2020-09-29 22:09:01 +0200

    new way

---
+++ src/page/404.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+const Container = styled.section`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+function IncorrectPage() {
+    return (
+        <Container>
+            <h1>404 page </h1>
+            <Link to="/">Home</Link>
+        </Container>
+    );
+}
+
+export default IncorrectPage;
