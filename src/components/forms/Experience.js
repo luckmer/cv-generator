2c488ad5da79d18cd24e19819bddf7fabeb67819
luckmer <woszczo04@gmail.com>
2020-09-30 22:27:54 +0200

    .

--- src/components/forms/Experience.js
+++ src/components/forms/Experience.js
@@ -2,135 +2,92 @@ import { nanoid } from "nanoid";
 import React, { useState } from "react";
 
 const initialState = {
-    workExperience: [],
-    education: [],
-    languageKnowledge: [],
-    certificatesEtc: [],
-    professionalProfile: "",
+    wE: [],
+    en: [],
+    lk: [],
+    ce: [],
+    pp: "",
 };
 
 function Experience() {
     const [state, setState] = useState(initialState);
     const [text, setText] = useState({
         id: nanoid(),
-        workText: "",
-        eduText: "",
-        languageText: "",
-        certificateText: "",
+        WT: "",
+        ED: "",
+        LT: "",
+        CT: "",
     });
     const handleChange = (e) => {
         setText({ ...text, [e.target.name]: e.target.value });
     };
     console.log(state);
-    const { languageText, workText, eduText, certificateText } = text;
-    const {
-        workExperience,
-        education,
-        languageKnowledge,
-        certificatesEtc,
-        professionalProfile,
-    } = state;
+    const { LT, WT, ED, CT } = text;
+    const { wE, en, lk, ce, pp } = state;
 
     const handleWorkSubmit = (e) => {
         e.preventDefault();
-        const newCv = {
-            id: nanoid(),
-            data: workText,
-        };
-        const test = workExperience.concat(newCv);
-        setState({
-            workExperience: test,
-            education,
-            languageKnowledge,
-            certificatesEtc,
-            professionalProfile,
-        });
+        const newCv = { id: nanoid(), data: WT };
+        const test = wE.concat(newCv);
+        setState({ wE: test, en, lk, ce, pp });
     };
 
     const handleEduSubmit = (e) => {
         e.preventDefault();
-        const newCv = {
-            id: nanoid(),
-            data: eduText,
-        };
-        const test = education.concat(newCv);
-        setState({
-            workExperience,
-            education: test,
-            languageKnowledge,
-            certificatesEtc,
-            professionalProfile,
-        });
+        const newCv = { id: nanoid(), data: ED };
+        const test = en.concat(newCv);
+        setState({ wE, en: test, lk, ce, pp });
     };
+
     const handleLangSubmit = (e) => {
         e.preventDefault();
-
-        const newCv = {
-            id: nanoid(),
-            data: languageText,
-        };
-        const test = languageKnowledge.concat(newCv);
-        setState({
-            workExperience,
-            education,
-            languageKnowledge: test,
-            certificatesEtc,
-            professionalProfile,
-        });
+        const newCv = { id: nanoid(), data: LT };
+        const test = lk.concat(newCv);
+        setState({ wE, en, lk: test, ce, pp });
     };
 
     const handleCertificateSubmit = (e) => {
         e.preventDefault();
-        const { certificateText } = text;
-
-        const newCv = {
-            id: nanoid(),
-            data: certificateText,
-        };
-        const test = certificatesEtc.concat(newCv);
-        setState({
-            workExperience,
-            education,
-            languageKnowledge,
-            certificatesEtc: test,
-            professionalProfile,
-        });
+        const { CT } = text;
+        const newCv = { id: nanoid(), data: CT };
+        const test = ce.concat(newCv);
+        setState({ wE, en, lk, ce: test, pp });
     };
     return (
         <div>
             <form onSubmit={handleWorkSubmit}>
                 <input
                     placeholder=" work experience"
-                    name="workExperience"
-                    value={text.workText}
-                    name="workText"
+                    name="wE"
+                    value={text.WT}
+                    name="WT"
                     onChange={handleChange}
                 />
                 <button type="submit">Submit</button>
             </form>
             <form onSubmit={handleEduSubmit}>
                 <input
-                    placeholder="education"
-                    value={text.eduText}
-                    name="eduText"
+                    placeholder="en"
+                    value={text.ED}
+                    name="ED"
                     onChange={handleChange}
                 />
                 <button type="submit">Submit</button>
             </form>
             <form onSubmit={handleLangSubmit}>
                 <input
-                    placeholder="languageKnowledge"
-                    value={text.languageText}
-                    name="languageText"
+                    placeholder="lk"
+                    value={text.LT}
+                    name="LT"
                     onChange={handleChange}
                 />
                 <button type="submit">Submit</button>
             </form>
             <form onSubmit={handleCertificateSubmit}>
                 <input
-                    placeholder="certificatesEtc"
-                    value={text.certificateText}
-                    name="certificateText"
+                    placeholder="ce"
+                    value={text.CT}
+                    name="CT"
                     onChange={handleChange}
                 />
                 <button type="submit">Submit</button>
