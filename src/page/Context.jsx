03dc8044d3f445097b8b8f597d4e59f31df531eb
luckmer <woszczo04@gmail.com>
2020-10-02 22:35:05 +0200

    tiny fix

--- src/page/Context.jsx
+++ src/page/Context.jsx
@@ -1,39 +1,22 @@
-import React, { useContext } from "react";
-import { StoreContext } from "../store/index";
-import Table from "../components/Table";
+import React from "react";
+import Skills from "../components/forms/Skills";
+import Experience from "../components/forms/Experience";
+import BasicData from "../components/forms/BasicData";
+
 import styled from "styled-components";
 
 const Container = styled.div`
     padding: 10vh 0 0 0;
 `;
 
-function Context() {
-    const {
-        clearTableData,
-        DATA: [table],
-    } = useContext(StoreContext);
-    const { data } = table;
-
-    const editTask = () => {};
-
+function Context({ match }) {
     return (
         <Container>
-            {data.map(({ title, id }) => (
-                <Table
-                    key={id}
-                    title={title}
-                    id={id}
-                    clearTableData={clearTableData}
-                    editTask={editTask}
-                />
-            ))}
+            <Experience match={match} />
+            <BasicData match={match} />
+            <Skills match={match} />
         </Container>
     );
 }
 
 export default Context;
-// <Container>
-//     <Experience match={match} />
-//     <BasicData match={match} />
-//     <Skills match={match} />
-// </Container>
