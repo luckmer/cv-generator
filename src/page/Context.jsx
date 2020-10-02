6ae00632f08174775cdfa30e8d441589f9deba4c
luckmer <woszczo04@gmail.com>
2020-10-02 21:32:29 +0200

    context

--- src/page/Context.jsx
+++ src/page/Context.jsx
@@ -1,22 +1,39 @@
-import React from "react";
-import Skills from "../components/forms/Skills";
-import Experience from "../components/forms/Experience";
-import BasicData from "../components/forms/BasicData";
-
+import React, { useContext } from "react";
+import { StoreContext } from "../store/index";
+import Table from "../components/Table";
 import styled from "styled-components";
 
 const Container = styled.div`
     padding: 10vh 0 0 0;
 `;
 
-function Context({ match }) {
+function Context() {
+    const {
+        clearTableData,
+        DATA: [table],
+    } = useContext(StoreContext);
+    const { data } = table;
+
+    const editTask = () => {};
+
     return (
         <Container>
-            <Experience match={match} />
-            <BasicData match={match} />
-            <Skills match={match} />
+            {data.map(({ title, id }) => (
+                <Table
+                    key={id}
+                    title={title}
+                    id={id}
+                    clearTableData={clearTableData}
+                    editTask={editTask}
+                />
+            ))}
         </Container>
     );
 }
 
 export default Context;
+// <Container>
+//     <Experience match={match} />
+//     <BasicData match={match} />
+//     <Skills match={match} />
+// </Container>
