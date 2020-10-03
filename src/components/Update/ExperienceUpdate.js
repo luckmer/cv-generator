f0088ea8a35dee7c236375cbc5c714be8a47ee01
luckmer <woszczo04@gmail.com>
2020-10-03 21:59:16 +0200

    experience update form

---
+++ src/components/Update/ExperienceUpdate.js
@@ -0,0 +1,69 @@
+import { useState, useContext } from "react";
+import { StoreContext } from "../../store/index";
+
+import { nanoid } from "nanoid";
+
+const initialState = {
+    wE: [],
+    en: [],
+    lk: [],
+    ce: [],
+    pp: "",
+};
+
+export default function ExperienceUpdate() {
+    const [state, setState] = useState(initialState);
+    const [text, setText] = useState({ WT: "", ED: "", LT: "", CT: "" });
+
+    const {
+        DATAUPDATE: [editData, setEditData],
+    } = useContext(StoreContext);
+
+    const handleChange = (e) => {
+        setText({ ...text, [e.target.name]: e.target.value });
+    };
+    const { basicData, skillsData } = editData;
+    const { LT, WT, ED, CT } = text;
+    const { wE, en, lk, ce, pp } = state;
+    const Data = basicData.concat(state);
+
+    const handleWorkSubmit = (e) => {
+        e.preventDefault();
+        const newCv = { id: nanoid(), data: WT };
+        const test = wE.concat(newCv);
+        setEditData({ experienceData: Data, basicData, skillsData });
+        setState({ wE: test, en, lk, ce, pp });
+    };
+
+    const handleEduSubmit = (e) => {
+        e.preventDefault();
+        const newCv = { id: nanoid(), data: ED };
+        const test = en.concat(newCv);
+        setEditData({ experienceData: Data, basicData, skillsData });
+        setState({ wE, en: test, lk, ce, pp });
+    };
+
+    const handleLangSubmit = (e) => {
+        e.preventDefault();
+        const newCv = { id: nanoid(), data: LT };
+        const test = lk.concat(newCv);
+        setEditData({ experienceData: Data, basicData, skillsData });
+        setState({ wE, en, lk: test, ce, pp });
+    };
+
+    const handleCertificateSubmit = (e) => {
+        e.preventDefault();
+        const newCv = { id: nanoid(), data: CT };
+        const test = ce.concat(newCv);
+        setEditData({ experienceData: Data, basicData, skillsData });
+        setState({ wE, en, lk, ce: test, pp });
+    };
+    return {
+        handleWorkSubmit,
+        handleEduSubmit,
+        handleLangSubmit,
+        handleCertificateSubmit,
+        handleChange,
+        text,
+    };
+}
