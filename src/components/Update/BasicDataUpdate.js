c6389c9fe71f6fe012059e252e6241e962558762
luckmer <woszczo04@gmail.com>
2020-10-03 21:59:26 +0200

    basicData update form

---
+++ src/components/Update/BasicDataUpdate.js
@@ -0,0 +1,32 @@
+import { useState, useContext } from "react";
+import { StoreContext } from "../../store/index";
+import { nanoid } from "nanoid";
+
+const initialState = {
+    id: nanoid(),
+    name: "",
+    surname: "",
+    dataOfBirth: "",
+    country: "",
+    livingPlace: "",
+    phoneNumber: "",
+};
+
+export default function BasicDataUpdate() {
+    const [state, setState] = useState(initialState);
+    const {
+        DATAUPDATE: [editData, setEditData],
+    } = useContext(StoreContext);
+
+    const { basicData, experienceData, skillsData } = editData;
+
+    const handleDataChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value });
+    };
+    const handleDataSubmit = (e) => {
+        e.preventDefault();
+        const test = basicData.concat(state);
+        setEditData({ basicData: test, experienceData, skillsData });
+    };
+    return { handleDataSubmit, handleDataChange, state };
+}
