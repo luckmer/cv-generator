9adfa0cc4a1e28ab179ab5cc7a723064bd0d414d
luckmer <woszczo04@gmail.com>
2020-10-03 21:59:49 +0200

    functionality

--- src/components/forms/Experience.js
+++ src/components/forms/Experience.js
@@ -1,62 +1,15 @@
-import React, { useState, useContext } from "react";
-import { nanoid } from "nanoid";
-import { StoreContext } from "../../store/index";
-
-const initialState = {
-    wE: [],
-    en: [],
-    lk: [],
-    ce: [],
-    pp: "",
-};
+import React from "react";
+import ExperienceUpdate from "../Update/ExperienceUpdate";
 
 function Experience() {
-    const [state, setState] = useState(initialState);
-    const [text, setText] = useState({ WT: "", ED: "", LT: "", CT: "" });
-
     const {
-        DATAUPDATE: [editData, setEditData],
-    } = useContext(StoreContext);
-
-    const handleChange = (e) => {
-        setText({ ...text, [e.target.name]: e.target.value });
-    };
-    const { basicData, skillsData } = editData;
-    const { LT, WT, ED, CT } = text;
-    const { wE, en, lk, ce, pp } = state;
-    const Data = basicData.concat(state);
-
-    const handleWorkSubmit = (e) => {
-        e.preventDefault();
-        const newCv = { id: nanoid(), data: WT };
-        const test = wE.concat(newCv);
-        setEditData({ experienceData: Data, basicData, skillsData });
-        setState({ wE: test, en, lk, ce, pp });
-    };
-
-    const handleEduSubmit = (e) => {
-        e.preventDefault();
-        const newCv = { id: nanoid(), data: ED };
-        const test = en.concat(newCv);
-        setEditData({ experienceData: Data, basicData, skillsData });
-        setState({ wE, en: test, lk, ce, pp });
-    };
-
-    const handleLangSubmit = (e) => {
-        e.preventDefault();
-        const newCv = { id: nanoid(), data: LT };
-        const test = lk.concat(newCv);
-        setEditData({ experienceData: Data, basicData, skillsData });
-        setState({ wE, en, lk: test, ce, pp });
-    };
-
-    const handleCertificateSubmit = (e) => {
-        e.preventDefault();
-        const newCv = { id: nanoid(), data: CT };
-        const test = ce.concat(newCv);
-        setEditData({ experienceData: Data, basicData, skillsData });
-        setState({ wE, en, lk, ce: test, pp });
-    };
+        handleWorkSubmit,
+        handleEduSubmit,
+        handleLangSubmit,
+        handleCertificateSubmit,
+        handleChange,
+        text,
+    } = ExperienceUpdate();
     return (
         <div>
             <form onSubmit={handleWorkSubmit}>
