b898a2d8cca289f1a43b30f05e9f33d130af4cbc
luckmer <woszczo04@gmail.com>
2020-10-03 21:59:42 +0200

    functionality

--- src/components/forms/BasicData.js
+++ src/components/forms/BasicData.js
@@ -1,33 +1,9 @@
-import React, { useState, useContext } from "react";
-import { StoreContext } from "../../store/index";
-import { nanoid } from "nanoid";
+import React from "react";
 
-const initialState = {
-    id: nanoid(),
-    name: "",
-    surname: "",
-    dataOfBirth: "",
-    country: "",
-    livingPlace: "",
-    phoneNumber: "",
-};
+import BasicDataUpdate from "../Update/BasicDataUpdate";
 
 function BasicData() {
-    const [state, setState] = useState(initialState);
-    const {
-        DATAUPDATE: [editData, setEditData],
-    } = useContext(StoreContext);
-
-    const { basicData, experienceData, skillsData } = editData;
-
-    const handleDataChange = (e) => {
-        setState({ ...state, [e.target.name]: e.target.value });
-    };
-    const handleDataSubmit = (e) => {
-        e.preventDefault();
-        const test = basicData.concat(state);
-        setEditData({ basicData: test, experienceData, skillsData });
-    };
+    const { handleDataSubmit, handleDataChange, state } = BasicDataUpdate();
     return (
         <form onSubmit={handleDataSubmit}>
             <input name="name" value={state.name} onChange={handleDataChange} />
