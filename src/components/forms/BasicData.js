f64db4ecd0d8da81533a0126e25e0f032bb8fa55
luckmer <woszczo04@gmail.com>
2020-10-04 18:42:27 +0200

    basic data form

--- src/components/forms/BasicData.js
+++ src/components/forms/BasicData.js
@@ -1,40 +1,142 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import { ContainerContext, FormContext, Form } from "../../css/FormDataStyles";
 import BasicDataUpdate from "../Update/BasicDataUpdate";
 
 function BasicData() {
-    const { handleDataSubmit, handleDataChange, state } = BasicDataUpdate();
+    const history = useHistory();
+    const { register, handleSubmit, errors } = useForm();
+    const {
+        handleDataSubmit,
+        handleDataChange,
+        state,
+        editData,
+    } = BasicDataUpdate();
+    const {
+        name,
+        surname,
+        dataOfBirth,
+        country,
+        livingPlace,
+        phoneNumber,
+    } = state;
+    const { basicData } = editData;
+    console.log(basicData.length);
+
+    if (basicData.length > 0) {
+        history.push("/experience");
+    }
     return (
-        <form onSubmit={handleDataSubmit}>
-            <input name="name" value={state.name} onChange={handleDataChange} />
-            <input
-                name="surname"
-                value={state.surname}
-                onChange={handleDataChange}
-            />
-            <input
-                type="text"
-                name="dataOfBirth"
-                value={state.dataOfBirth}
-                onChange={handleDataChange}
-            />
-            <input
-                name="country"
-                value={state.country}
-                onChange={handleDataChange}
-            />
-            <input
-                name="livingPlace"
-                value={state.livingPlace}
-                onChange={handleDataChange}
-            />
-            <input
-                name="phoneNumber"
-                value={state.phoneNumber}
-                onChange={handleDataChange}
-            />
-            <button type="submit">Submit</button>
-        </form>
+        <ContainerContext>
+            <FormContext>
+                <Form onSubmit={handleSubmit(handleDataSubmit)}>
+                    <div>
+                        <label>Name*</label>
+                        <input
+                            ref={register({
+                                pattern: /^[^\s]+( [^\s]+)+$/i,
+                                required: true,
+                            })}
+                            name="name"
+                            type="text"
+                            onChange={handleDataChange}
+                        />
+                        <small>first and last name</small>
+                        <br />
+                        {errors.name && <span>This field is required</span>}
+                    </div>
+
+                    <div>
+                        <label>Surname*</label>
+                        <input
+                            ref={register({
+                                pattern: /^[^\s]+( [^\s]+)+$/i,
+                                required: true,
+                            })}
+                            name="surname"
+                            onChange={handleDataChange}
+                        />
+                        <small>first and last name</small>
+                        <br />
+                        {errors.surname && <span>This field is required</span>}
+                    </div>
+
+                    <div>
+                        <label>Data of Birth*</label>
+                        <input
+                            ref={register({
+                                pattern: /^(?:0[1-9]|[12]\d|3[01])([\/. ])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/g,
+                                required: true,
+                            })}
+                            type="text"
+                            name="dataOfBirth"
+                            onChange={handleDataChange}
+                        />
+                        <small>DD/MM/YYYY</small>
+                        <br />
+                        {errors.dataOfBirth && (
+                            <span>This field is required</span>
+                        )}
+                    </div>
+
+                    <div>
+                        <label>Country*</label>
+                        <input
+                            name="country"
+                            onChange={handleDataChange}
+                            ref={register({ required: true })}
+                        />
+                        <br />
+                        {errors.country && <span>This field is required</span>}
+                    </div>
+
+                    <div>
+                        <label>residence*</label>
+                        <input
+                            name="livingPlace"
+                            onChange={handleDataChange}
+                            ref={register({ required: true })}
+                        />
+                        <br />
+                        {errors.livingPlace && (
+                            <span>This field is required</span>
+                        )}
+                    </div>
+
+                    <div>
+                        <label>phone number*</label>
+                        <input
+                            ref={register({
+                                pattern: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g,
+                                required: true,
+                            })}
+                            placeholder="phone number "
+                            name="phoneNumber"
+                            type="number"
+                            onChange={handleDataChange}
+                        />
+                        <small>### ### ####</small>
+                        {errors.phoneNumber && (
+                            <span>This field is required</span>
+                        )}
+                    </div>
+                    <button
+                        type="submit"
+                        disabled={
+                            name == "" ||
+                            surname == "" ||
+                            dataOfBirth == "" ||
+                            country == "" ||
+                            livingPlace == "" ||
+                            phoneNumber == ""
+                        }
+                    >
+                        Submit
+                    </button>
+                </Form>
+            </FormContext>
+        </ContainerContext>
     );
 }
 
