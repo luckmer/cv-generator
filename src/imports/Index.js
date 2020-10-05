95dcd7fc5f468f103d99f0b59810267a740a1ae6
luckmer <woszczo04@gmail.com>
2020-10-05 21:44:12 +0200

    imports

---
+++ src/imports/Index.js
@@ -0,0 +1,25 @@
+//pages
+export { default as IncorrectPage } from "../page/404";
+export { default as Context } from "../page/Context";
+export { default as Details } from "../page/Details";
+export { default as Download } from "../page/Download";
+export { default as ExperienceContext } from "../page/Experience";
+export { default as LandingPage } from "../page/LandingPage";
+export { default as Skill } from "../page/Skills";
+//hooks
+export { default as DetailFilter } from "../hooks/DetailFilter";
+export { default as OpenHook } from "../hooks/OpenHook";
+//components
+export { default as Table } from "../components/Table";
+export { default as NavBar } from "../components/NavBar";
+export { default as CreateCV } from "../components/CreateCv";
+//components ->update
+export { default as BasicDataUpdate } from "../components/Update/BasicDataUpdate";
+export { default as ExperienceUpdate } from "../components/Update/ExperienceUpdate";
+export { default as SkillsUpdate } from "../components/Update/SkillsUpdate";
+//components ->forms -> context
+export { default as BasicData } from "../components/forms/BasicData";
+export { default as Experience } from "../components/forms/Experience";
+export { default as Skills } from "../components/forms/Skills";
+// components -> filter
+export { default as Filter } from "../components/Filter/Filter";
