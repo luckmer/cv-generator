81d274524e3e96ceb10f5c32eb7381296bae78da
luckmer <woszczo04@gmail.com>
2020-10-05 21:44:17 +0200

    open hook

---
+++ src/hooks/OpenHook.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function OpenHook() {
+    return useState({
+        o: false,
+        t: false,
+        th: false,
+        f: false,
+    });
+}
