a99c2e112547b800c6e2624776a77ab3c6e3ff9e
luckmer <woszczo04@gmail.com>
2020-10-05 21:47:32 +0200

    download panel update

--- src/page/Download.jsx
+++ src/page/Download.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Page, Text, Document, PDFViewer } from "@react-pdf/renderer";
+
+import styled from "styled-components";
+const Container = styled.div`
+    iframe {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+    }
+`;
 
 function Download() {
-    return <div></div>;
+    return (
+        <Container>
+            <PDFViewer>
+                <Document>
+                    <Page>
+                        <Text>soon</Text>
+                    </Page>
+                </Document>
+            </PDFViewer>
+        </Container>
+    );
 }
 
 export default Download;
