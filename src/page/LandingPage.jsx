d96c9224f928f1b3ca86d33272b6e430a8efccfc
luckmer <woszczo04@gmail.com>
2020-10-05 21:48:53 +0200

    functionality 🎈

--- src/page/LandingPage.jsx
+++ src/page/LandingPage.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useContext } from "react";
-import { Container, CreateContext } from "../css/LandingPageStyle";
-import CreateCV from "../components/CreateCv";
+import {
+    Container,
+    CreateContext,
+    TableContainer,
+} from "../css/LandingPageStyle";
 import { StoreContext } from "../store";
-import Table from "../components/Table";
-import styled from "styled-components";
-import Filter from "../components/Filter/Filter";
-
-const TableContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-    padding: 10px 10vw 10px 10vw;
-    opacity: ${({ controlOpen }) => (controlOpen ? 0 : 1)};
-`;
+import { Filter, Table, CreateCV } from "../imports/Index";
 
 function LandingPage() {
     const [open, setOpen] = useState(false);
