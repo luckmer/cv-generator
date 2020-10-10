59b15d8c482117986b7cd0ee67349a1533c85c00
luckmer <woszczo04@gmail.com>
2020-10-10 17:14:14 +0200

    soon

--- src/page/Download.jsx
+++ src/page/Download.jsx
@@ -1,6 +1,5 @@
-import React, { useContext } from "react";
-import { Page, Text, Document, PDFViewer } from "@react-pdf/renderer";
-import { StoreContext } from "../store/index";
+import React from "react";
+import { Page,  Document, PDFViewer } from "@react-pdf/renderer";
 
 import styled from "styled-components";
 const Container = styled.div`
@@ -12,44 +11,17 @@ const Container = styled.div`
 `;
 
 function Download() {
-    const {
-        ID,
-        DATA: [table],
-    } = useContext(StoreContext);
-    const { data, BasicData } = table;
 
     return (
         <Container>
-            {data.map((task) => {
-                if (ID === task.id) {
-                    return (
-                        <PDFViewer key={task.id}>
-                            <Document>
-                                <Page>
-                                    <Text>{task.title}</Text>
-                                    <Text>
-                                        {task.BasicData.map((task) => (
-                                            <Text key={task.id}>
-                                                <Text>
-                                                    Country : {task.country}
-                                                    dataOfBirth :
-                                                    {task.dataOfBirth}
-                                                    livingPlace :
-                                                    {task.livingPlace}
-                                                    name : {task.name}
-                                                    surname: {task.surname}
-                                                    phoneNumber:
-                                                    {task.phoneNumber}
-                                                </Text>
-                                            </Text>
-                                        ))}
-                                    </Text>
-                                </Page>
-                            </Document>
-                        </PDFViewer>
-                    );
-                }
-            })}
+            <PDFViewer>
+                <Document>
+                    <Page>
+                
+            
+                    </Page>
+                </Document>
+            </PDFViewer>
         </Container>
     );
 }
