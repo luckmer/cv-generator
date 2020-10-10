716c2d5d9c9000feb4ac9d5186a1958152dce8eb
luckmer <woszczo04@gmail.com>
2020-10-10 17:13:54 +0200

    soon upadate

--- src/components/Update/SkillsUpdate.js
+++ src/components/Update/SkillsUpdate.js
@@ -16,12 +16,24 @@ export default function SkillsUpdate() {
     const {
         DATAUPDATE: [editData, setEditData],
     } = useContext(StoreContext);
-    const [text, setText] = useState({ as: "", ay: "", ls: "", pd: "" });
 
+    const [text, setText] = useState({ as: ""});
     const { basicData, experienceData } = editData;
     const { abilities, Activity, links, personalData } = state;
     const Data = basicData.concat(state);
-    const { as, ay, ls } = text;
+
+    
+    const [activeState, setActiveState] = useState({ay:""})
+    const [linkState ,setLinkState] = useState({ls:""})
+    const [personData ,setPersonData] = useState({pd:""})
+
+    //change
+    const handleChange = (e) => setText({ ...text, [e.target.name]: e.target.value });
+    const handleActiveChange = e => {
+        setActiveState({...activeState,[e.target.name]:e.target.value})
+    }
+    const handleLinkChange =e=> setLinkState({...linkState,[e.target.name]:e.target.value})
+    const handlePersonChange =e=> setPersonData({...personData ,[e.target.name]:e.target.value})
 
     useEffect(() => {
         const store = JSON.parse(localStorage.getItem("state"));
@@ -32,13 +44,10 @@ export default function SkillsUpdate() {
         localStorage.setItem("stateData", JSON.stringify(state));
     }, [state]);
 
-    const handleChange = (e) => {
-        setText({ ...text, [e.target.name]: e.target.value });
-    };
 
     const abilitiesSubmit = () => {
-        if (as) {
-            const newData = { id: nanoid(), data: as };
+        if (text) {
+            const newData = { id: nanoid(), data: text };
             const test = abilities.concat(newData);
             setEditData({ experienceData, basicData, skillsData: Data });
             setState({ abilities: test, Activity, links, personalData });
@@ -47,24 +56,30 @@ export default function SkillsUpdate() {
     };
 
     const activitySubmit = () => {
-        if (ay) {
-            const newData = { id: nanoid(), data: ay };
+        if (activeState) {
+            const newData = { id: nanoid(), data: activeState };
             const test = Activity.concat(newData);
             setEditData({ experienceData, basicData, skillsData: Data });
             setState({ abilities, Activity: test, links, personalData });
         }
-        setText({ ay: "" });
+        setActiveState({ ay: "" });
     };
 
     const linksSubmit = () => {
-        if (ls) {
-            const newData = { id: nanoid(), data: ls };
+        if (linkState) {
+            const newData = { id: nanoid(), data: linkState };
             const test = links.concat(newData);
             setEditData({ experienceData, basicData, skillsData: Data });
             setState({ abilities, Activity, links: test, personalData });
         }
     };
     return {
+        handlePersonChange,
+        personData,
+        handleLinkChange,
+        linkState,
+        activeState,
+        handleActiveChange,
         abilitiesSubmit,
         handleChange,
         activitySubmit,
