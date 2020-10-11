525d5d2ddb1d96b7de63ad84f7b5103a864c2bf9
luckmer <woszczo04@gmail.com>
2020-10-11 12:17:42 +0200

    palet

---
+++ src/css/colors.js
@@ -0,0 +1,14 @@
+export default {
+    white:"#fff",
+    darkBlue:"#212529",
+    lightDarkBlue:"#2c313a",
+    lightWhite:"#e5e6e6",
+    lessWhite:"#cccccc",
+    lightGrey:"#666666",
+    smallGrey:"#999999",
+    lightTomato:"#e8474c",
+    lighterWhite:"#f0f0f0",
+    clearWhite:"#d8d8d8",
+    black:"#000008",
+    grey:"grey"
+}
