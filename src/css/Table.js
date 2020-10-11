d2e5c81c2aef8399dd9c5e91114ba37f4512d555
luckmer <woszczo04@gmail.com>
2020-10-11 12:19:21 +0200

    table css update

--- src/css/Table.js
+++ src/css/Table.js
@@ -1,13 +1,9 @@
 import styled from "styled-components";
-
+import colors from "./colors"
 export const TableDesign = styled.table`
     box-shadow: rgba(0, 0, 0, 0.08) 0px 3px 6px;
     margin: 10px 0 10px 0;
-    display: flex;
-    justify-content: center;
-    text-algin: center;
-    width: 100%;
-    border-bottom: 1px solid #d8d8d8;
+    border-bottom: 1px solid ${colors.clearWhite};
     p {
         width: 100%;
     }
@@ -15,16 +11,29 @@ export const TableDesign = styled.table`
     td {
         width: 100vw;
         padding: 20px;
-        background-color: #ffffff;
-        color: #212529;
+        background-color: ${colors.white};
+        color: ${colors.darkBlue};
     }
     button {
-        background-color: #e8474c;
-        color: white;
+        background-color: ${colors.lightTomato};
+        color: ${colors.white};
         font-weight: bold;
         border-radius: 20px;
         margin: 0 10px 0 10px;
         padding: 5px 10px 5px 10px;
         border: none;
+    }
+    @media screen and (max-width: 940px) {
+        width:100%;
+        th,td{
+            width: calc(100% - 15px);
+            display:flex;
+            align-items:center;
+            flex-direction:column;
+        }
+        button{
+            margin:10px 0 0 0 ;
+            border-radius:5px;
+        }
     }
 `;
