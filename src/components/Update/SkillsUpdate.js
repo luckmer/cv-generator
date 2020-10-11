49321b219cd43930e5e7b214f8acd7cabd4e10c4
luckmer <woszczo04@gmail.com>
2020-10-11 12:19:13 +0200

    skills update**

--- src/components/Update/SkillsUpdate.js
+++ src/components/Update/SkillsUpdate.js
@@ -17,23 +17,18 @@ export default function SkillsUpdate() {
         DATAUPDATE: [editData, setEditData],
     } = useContext(StoreContext);
 
-    const [text, setText] = useState({ as: ""});
     const { basicData, experienceData } = editData;
     const { abilities, Activity, links, personalData } = state;
     const Data = basicData.concat(state);
 
-    
-    const [activeState, setActiveState] = useState({ay:""})
-    const [linkState ,setLinkState] = useState({ls:""})
-    const [personData ,setPersonData] = useState({pd:""})
 
-    //change
-    const handleChange = (e) => setText({ ...text, [e.target.name]: e.target.value });
-    const handleActiveChange = e => {
-        setActiveState({...activeState,[e.target.name]:e.target.value})
-    }
-    const handleLinkChange =e=> setLinkState({...linkState,[e.target.name]:e.target.value})
-    const handlePersonChange =e=> setPersonData({...personData ,[e.target.name]:e.target.value})
+    //test
+    const [text,setText]= useState({
+        as:"",ay:"",ls:"",pd:"",name:"",location:"",startDate:"",endDate:"",
+        description:"",
+    })
+    const {as,ay,ls,name,location,startDate,endDate,description }= text
+    const handleChange = e => setText({...text , [e.target.name]:e.target.value})
 
     useEffect(() => {
         const store = JSON.parse(localStorage.getItem("state"));
@@ -44,42 +39,39 @@ export default function SkillsUpdate() {
         localStorage.setItem("stateData", JSON.stringify(state));
     }, [state]);
 
-
+    console.log(text)
     const abilitiesSubmit = () => {
-        if (text) {
-            const newData = { id: nanoid(), data: text };
+        if (text.as) {
+            const newData = { id: nanoid(), data: as };
             const test = abilities.concat(newData);
             setEditData({ experienceData, basicData, skillsData: Data });
             setState({ abilities: test, Activity, links, personalData });
         }
-        setText({ as: "" });
+        setText({  as:"",ay:"",ls:"",pd:"",name:"",location:"",startDate:"",endDate:"",
+        description:""})
     };
-
     const activitySubmit = () => {
-        if (activeState) {
-            const newData = { id: nanoid(), data: activeState };
+        if (text.ay) {
+            const newData = { id: nanoid(), data: ay, name , location , startDate , endDate };
             const test = Activity.concat(newData);
             setEditData({ experienceData, basicData, skillsData: Data });
             setState({ abilities, Activity: test, links, personalData });
         }
-        setActiveState({ ay: "" });
+        setText({  as:"",ay:"",ls:"",pd:"",name:"",location:"",startDate:"",endDate:"",
+        description:""})
     };
 
     const linksSubmit = () => {
-        if (linkState) {
-            const newData = { id: nanoid(), data: linkState };
+        if (text.ls) {
+            const newData = { id: nanoid(), data: ls,description  };
             const test = links.concat(newData);
             setEditData({ experienceData, basicData, skillsData: Data });
             setState({ abilities, Activity, links: test, personalData });
+            setText({  as:"",ay:"",ls:"",pd:"",name:"",location:"",startDate:"",endDate:"",
+            description:""})
         }
     };
     return {
-        handlePersonChange,
-        personData,
-        handleLinkChange,
-        linkState,
-        activeState,
-        handleActiveChange,
         abilitiesSubmit,
         handleChange,
         activitySubmit,
