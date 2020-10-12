29c281b1f3d8b39264538ccbb523dfab3f07b1e4
luckmer <woszczo04@gmail.com>
2020-10-12 20:18:11 +0200

    Download js update

--- src/page/Download.jsx
+++ src/page/Download.jsx
@@ -1,14 +1,80 @@
 import React,{useContext} from "react";
 import { Page,Text,Document, PDFViewer } from "@react-pdf/renderer";
+import {Container} from "../css/Downloadcss"
+import styled from '@react-pdf/styled-components';
 import {StoreContext} from "../store/index"
-import styled from "styled-components";
-const Container = styled.div`
-    iframe {
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-    }
+
+
+const Body = styled.Page`
+  padding-top: 35px;
+  padding-bottom: 65px;
+  padding-right: 35px;
+  padding-left: 35px;
 `;
+
+const Header = styled.Text`
+  color: grey;
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+const Contact = styled.Text `
+    margin: 12px;
+    font-size: 14px;
+    text-align:center;
+    flexDirection:column;
+    font-family: 'Times-Roman';
+`
+const BornData = styled.Text `
+    font-size: 14px;
+    text-align:center;
+    flexDirection:column;
+    font-family: 'Times-Roman';
+    margin: 4px;
+`
+const Margin = styled.Text `
+    margin:12px;
+`
+const TypeView = styled.Text `
+  color: grey;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom:5px;
+ border:1px solid black;
+`
+
+
+const Test = ({task})=>{
+    return(
+        <>
+        <Contact>
+            {task.name} {task.surname}
+        </Contact>
+        <BornData>
+            Data of birth: {task.dataOfBirth}
+        </BornData>
+        <BornData>
+            country: {task.country}
+        </BornData>
+        <TypeView>
+            Work Experience
+        </TypeView>
+        <TypeView>
+            Education
+        </TypeView>
+        <TypeView>
+            knowledge of languages
+        </TypeView>
+        <TypeView>
+            training courses certificates
+        </TypeView>
+        <TypeView>
+            interests
+        </TypeView>
+        </>
+    )
+}
 
 function Download() {
     const {
@@ -19,20 +85,12 @@ function Download() {
         <Container>
             <PDFViewer>
                 <Document>
-                    <Page>
-                    <Text>
-                        {editData.basicData.map((task)=>(
-                            <Text key ={task.id}>
-                                country : {task.country}
-                                data of Birth : {task.dataOfBirth}
-                                living place : {task.livingPlace}
-                                name : {task.name}
-                                surname : {task.surname}
-                                phone number :{task.phoneNumber}
-                            </Text>
-                        ))}
-                    </Text>
-                    </Page>
+                    <Body>
+                        <Header> - CV -</Header>
+                            {editData.basicData.map((task)=>(
+                                <Test task ={task} key={task.id}/>
+                            ))}
+                    </Body>
                 </Document>
             </PDFViewer>
         </Container>
