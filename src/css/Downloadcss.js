25c6db812aca7df146fe85d33eb6fce35582b1fb
luckmer <woszczo04@gmail.com>
2020-10-12 20:17:54 +0200

    downloadCSS

---
+++ src/css/Downloadcss.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+
+export const Container = styled.div`
+    iframe {
+        margin-top:4em;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+    }
+`;
